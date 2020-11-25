f2df08f18f7dee0a9428762696f9748f8c055e22
Denisse huerta <denissehuertac@gmail.com>
2020-11-24 20:47:32 -0600

    Código mejorado, reemplazo de función por clousure

--- Clima9-10/ClimaManager.swift
+++ Clima9-10/ClimaManager.swift
@@ -25,20 +25,22 @@ struct ClimaManager {
             //2.- Crear la URL session
             let session = URLSession(configuration: .default)
             //3.- Asignar una tarea a la session
-            let tarea = session.dataTask(with: url, completionHandler: handle(data:respuesta:error:))
+            //let tarea = session.dataTask(with: url, completionHandler: handle(data:respuesta:error:))
+            let tarea = session.dataTask(with: url) { (data, request, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let datosSeguros = data {
+                    //Decodificar el obj JSON
+                    let dataString = String(data: datosSeguros, encoding: .utf8)
+                    print(dataString!)
+                }
+            }
             //4.- Empezar la tarea
             tarea.resume()
         }
         
     }
-    func handle(data:Data?, respuesta:URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let datosSeguros = data {
-            let dataString = String(data: datosSeguros, encoding: .utf8)
-             print(dataString!)
-        }
-    }
+    
 }
